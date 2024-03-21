f0934d4022255b60f18c5a4db30ba5ca146720f5
KHJ <Hyzee2>
2024-03-21 19:00:52 +0900

    목요일

--- Day1/src/DataStructureDay03/Insertion.java
+++ Day1/src/DataStructureDay03/Insertion.java
@@ -4,38 +4,36 @@ import java.util.Random;
 import java.util.random.*;
 
 public class Insertion {
-	int cnt = 0;
+	
 	int temp=0;
 	
 	void change(int[] arr) { // 자리 값을 비교해서 바꿔주는 메서드 
-		// 1. i의 값을 선택해준다. i의 값을  temp 변수에 넣어준다. 
-		// 2. i의 바로 왼쪽(i-1)에 있는 값과 비교한다. 
-		// 3. 왼쪽 값이 더 크면 큰 값을 i에 대입한다. 
-		// 4. 계속 비교하다가 왼쪽 값이 더 작으면 그 위치에 temp 변수 값을 넣어준다. 
-		// * i의 값은 1부터 arr.length길이만큼
-		
-		for(int p=1; p<arr.length; p++) {
-			temp = arr[p];
-			if(arr[p] < arr[p-1]) {
-				arr[p] = arr[p-1];
-				cnt++;
-			}else {
-				arr[p] = temp;
-			}
-		}
 
 	}
 
 	void insertionSort(int[] arr) {
-//		int cnt = 0;
-		int ch = 0;
-		for (int i = 0; i < arr.length; i++) {
-			change(arr);
-			ch++;
+		int cnt = 0;
+		//int ch = 0;
+		int i = 0;
+		int j = 0;
+		
+		for (i = 1; i < arr.length; i++) {
+			
+			temp = arr[i]; // 우선 비교기준이 되는 값을 temp에 담아두기 
+			
+			for(j=i-1; j>=0 && arr[j]>temp; j--) { // j는 i의 값보다 1 작은 인덱스 / j는 0이상이어야 하고, 비교기준이 되는 i의 값보다 더 큰 경우 / j의 값은 점점 감소
+				arr[j+1] = arr[j]; // j 위치의 값을 오른쪽으로 한 칸 옮긴다.  
+			}	
+			
+			arr[j] = temp; // 최종적으로 비교기준이 되는 i위치의 값을 마지막 j 위치의 값으로 넣어준다. 
+			
+			cnt++;
+			
+			//ch++;
 		}
 
 		System.out.println("실제로 자리 이동 일어난 횟수는 :" + cnt);
-		System.out.println("실제로 값을 비교한 횟수는 :" + ch);
+		//System.out.println("실제로 값을 비교한 횟수는 :" + ch);
 	}
 
 	public static void main(String[] args) {
